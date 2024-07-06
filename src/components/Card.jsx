0c699a2be6cbb946db66a1ffea66f4bfef9b741f
Vatsal Chauhan <112819416+Vatsal-Chauhan024@users.noreply.github.com>
2024-07-06 14:44:02 +0530

    project updates

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -8,7 +8,7 @@ const Card = ({ imgProps, titleProps, contentProps, linkProps }) => {
     return (
         <>
         <div className='max-w-full md:max-w-80 lg:max-w-md xl:max-w-sm w-full  rounded-xl  backdrop-blur-2xl bg-tertiaryColor dark:bg-tertiaryColor/5 flex flex-col gap-5 overflow-hidden shadow-customshadow' data-aos = "fade-in">
-            <ImageField src={imgProps} alt="error-icon" className=" object-fill aspect-square h-72"/>
+            <ImageField src={imgProps} alt="error-icon" className=" md:object-cover  aspect-square h-72"/>
            <div className='flex px-4 pb-4 flex-col gap-4  justify-between h-1/2 '>
             <h2 className='text-mainbg/85 dark:text-tertiaryColor/80 tracking-wide capitalize text-xl font-poppinsB '>{titleProps}</h2>
             <p className='text-mainbg/60 dark:text-tertiaryColor/40 font-poppinsR text-justify text-sm md:text-base'>{contentProps}</p>
--- src/data/ProjectArray.js
+++ src/data/ProjectArray.js
@@ -1,61 +1,95 @@
-import todo from "../assets/illustrations/todo.png"
-import ecommerce from "../assets/illustrations/ecommerce.png"
-import portfolio from "../assets/illustrations/portfolio.png"
-import movie from "../assets/illustrations/movie.png"
-import hotel from "../assets/illustrations/hotel.png"
-import assist from "../assets/illustrations/cassist.png"
-import book from "../assets/illustrations/books.png"
-import land from "../assets/illustrations/land.png"
+import todo from "../assets/illustrations/todo.png";
+import ecommerce from "../assets/illustrations/ecommerce.png";
+import portfolio from "../assets/illustrations/portfolio.png";
+import hotel from "../assets/illustrations/hotel.png";
+import assist from "../assets/illustrations/cassist.png";
+import book from "../assets/illustrations/books.png";
+import cars from "../assets/illustrations/cars.png";
+import redux from "../assets/illustrations/redux.png";
+import qrcode from "../assets/illustrations/qrcode.png";
+import signup from "../assets/illustrations/signup.png";
+import api from "../assets/illustrations/api.png"
 
 const CardArray = [
   {
     itemIcon: todo,
     itemTitle: "Todo List",
-    itemContent: "A todo list made using latest Tech Stack(MERN), and uploaded to git using github Commands. Fully Responsive for small screen and large Screen devices, efficient implementation using react functional component using v6 routings.",
-    itemLink: "https://github.com/Vatsal-Chauhan024/todo-List"
+    itemContent:
+      "A todo list made using latest Tech Stack(MERN), and uploaded to git using github Commands. Fully Responsive for small screen and large Screen devices, efficient implementation using react functional component using v6 routings.",
+    itemLink: "https://github.com/Vatsal-Chauhan024/todo-List",
   },
   {
     itemIcon: ecommerce,
     itemTitle: "E-Commerce",
-    itemContent: "Designed and developed a clean and modern website using HTML, CSS, and React.js. Deployed on GitHub pages via GitHub Commands.",
-    itemLink: "https://github.com/Vatsal-Chauhan024/E-Commerce"
+    itemContent:
+      "Designed and developed a clean and modern website using HTML, CSS, and React.js. Deployed on GitHub pages via GitHub Commands.",
+    itemLink: "https://github.com/Vatsal-Chauhan024/E-Commerce",
   },
   {
     itemIcon: portfolio,
     itemTitle: "Portfolio",
-    itemContent: "Complete Personal Portfolio from scratch using Html, CSS, React.js and fully responsive to all devices. Full fledged responsive website hosted on netlify and uploaded on GitHub pages using Github commands.",
-    itemLink: "https://vatsalweb.netlify.app"
-  },
-  {
-    itemIcon: movie,
-    itemTitle: "Movie Recommendation System",
-    itemContent: "Complete Movie Recommendation System from scratch using Html, CSS, JavaScript, Django and fully responsive to all devices and worked as Frontend Developer.",
-    itemLink: "https://github.com/Vatsal-Chauhan024/Movie_Recommendation-Algorithm"
+    itemContent:
+      "Complete Personal Portfolio from scratch using Html, CSS, React.js and fully responsive to all devices. Full fledged responsive website hosted on netlify and uploaded on GitHub pages using Github commands.",
+    itemLink: "https://vatsalweb.netlify.app",
   },
   {
     itemIcon: hotel,
-    itemTitle: "Hotel Recommendation System",
-    itemContent: "Complete Hotel Recommendation System from scratch using Html, CSS, JavaScript, Django and fully responsive to all devices and worked as Front end Developer.",
-    itemLink:"https://github.com/Vatsal-Chauhan024/Hotel-Recommendation-System"
+    itemTitle: "Hotel Recommendation System (Group Project)",
+    itemContent:
+      "Complete Hotel Recommendation System from scratch using Html, CSS, JavaScript, Django and fully responsive to all devices and worked as Front end Developer.",
+    itemLink:
+      "https://github.com/Vatsal-Chauhan024/Hotel-Recommendation-System",
   },
   {
     itemIcon: assist,
     itemTitle: "CASSIST",
-    itemContent: "Worked in the industrial environment along with the Web Development team and worked on the Frontend Development using React.js  Tailwind CSS.",
-    itemLink: "https://www.doctalks.io/"
+    itemContent:
+      "Worked in the industrial environment along with the Web Development team and worked on the Frontend Development using React.js  Tailwind CSS.",
+    itemLink: "https://www.doctalks.io/",
   },
   {
     itemIcon: book,
     itemTitle: "Web Based Book Store App",
-    itemContent: "Developed Fully Responsive Complete Book Store App using React JS, Tailwind CSS, Backend using Firebase. Json Placeholder API, using the ContextAPI or UseContext concept of React.",
-    itemLink: "https://github.com/Vatsal-Chauhan024/Book-Store-Web-App"
+    itemContent:
+      "Developed Fully Responsive Complete Book Store App using React JS, Tailwind CSS, Backend using Firebase. Json Placeholder API, using the ContextAPI or UseContext concept of React.",
+    itemLink: "https://github.com/Vatsal-Chauhan024/Book-Store-Web-App",
   },
   {
-    itemIcon: land,
-    itemTitle: "BlockChain Based Land Registration System",
-    itemContent: "Worked as Frontend Developer along with one other member to implement the efficient UI Design using HTML5, CSS3, JS. Secured Second Rank in overall Gujarat (SSIP Hackathon).",
-    itemLink: "#"
-  }
+    itemIcon: cars,
+    itemTitle: "Car Management Dashboard",
+    itemContent:
+      "Developed MERN Book Store: React, Tailwind CSS, MongoDB backend. Features private routes, CRUD ops, responsive design, deployed for user-friendly experience.",
+    itemLink: "https://github.com/Vatsal-Chauhan024/Car-dashboard",
+  },
+  {
+    itemIcon: redux,
+    itemTitle: "Redux Functionality Project",
+    itemContent:
+      "Built Shopping Cart with Redux Toolkit and Tailwind CSS for efficient state management and modern UI. Led frontend development, ensuring seamless user experience and dynamic cart functionality.",
+    itemLink: "https://github.com/Vatsal-Chauhan024/Redux-project",
+  },
+  {
+    itemIcon: qrcode,
+    itemTitle: "Qr-Code Generator",
+    itemContent:
+      "Create a responsive QR code generator with React, Tailwind CSS, and Redux Toolkit. Input text or URL in a form to dynamically generate QR codes. Ensures a user-friendly experience with modern UI design.",
+    itemLink: "https://github.com/Vatsal-Chauhan024/QR-Generator",
+  },
+  {
+    itemIcon: api,
+    itemTitle: "Custom API Testing with Postman (Mini Project)",
+    itemContent: `
+Develop a custom API testing project using Postman, focusing on CRUD operations. Utilize Postman's features to create, read, update, and delete data via HTTP requests. Ensure thorough testing and validation of API endpoints for reliable functionality and performance.
+`,
+    itemLink: "https://github.com/Vatsal-Chauhan024/Api-testing-postman",
+  },
+  {
+    itemIcon: signup,
+    itemTitle: "Signup/SignIn using JWT",
+    itemContent:
+      "Implement secure user authentication with JWT using MERN stack and Tailwind CSS. Enable seamless signup and signin processes, ensuring robust data encryption and a sleek, responsive interface for optimal user interaction.",
+    itemLink: "https://github.com/Vatsal-Chauhan024/Sigup-using-JWT",
+  },
 ];
 
 export default CardArray;
